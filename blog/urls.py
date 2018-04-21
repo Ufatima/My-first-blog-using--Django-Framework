1c81f8944dce4aba6f7da48447e8ce2f1cb33584
ummulfatima <hoory1020@gmail.com>
2018-04-21 12:20:46 +0300

    Changed the HTML for the site.

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from . import views
+from django.shortcuts import render
+
+#importing Django's function url and all of our views from the blog application.
+
+urlpatterns = [
+    url(r'^$', views.post_list, name='post_list'),
+]
